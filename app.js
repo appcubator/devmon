28d6964afb6e3b9d3d660dd44cf792e8d8db5dcf
Karan Sikka <kssworld93@gmail.com>
2014-03-20 04:42:41 -0400

    officially not sure if this works

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ var devmon_log = function(s) {
     console.log('[Appcubator] ' + s);
 };
 
+var updateCode = function (tarpath, callback) {
+    child_process.exec('tar -xvf '+tarpath, function(err, stdout, stderr) {
+        if (err) devmon_log(err);
+        else {
+            devmon_log('Code updated. Tar output:');
+            devmon_log(stdout);
+            devmon_log(stderr);
+            callback();
+        }
+    });
+};
+
 var createApp = function (spawnConfigs, proxyConfigs) {
     var app = express();
     app.configure(function(){
@@ -20,17 +32,24 @@ var createApp = function (spawnConfigs, proxyConfigs) {
         res.send('Devmon configuration panel goes here.');
     });
 
-    var updateCode = function (tarpath, callback) {
-        child_process.exec('tar -xvf '+tarpath, function(err, stdout, stderr) {
-            if (err) devmon_log(err);
-            else {
-                devmon_log('Code updated. Tar output:');
-                devmon_log(stdout);
-                devmon_log(stderr);
-                callback();
-            }
-        });
-    };
+    // kill a process and remove the config entry
+    app.post('/conf/spawn/:pid/remove', function(req, res) {
+        var spawnConf = _.find(spawnConfigs, function(c) { return c.child.pid === req.params.pid });
+        spawnConf.child.kill('SIGINT');
+        spawnConf.child.disconnect();
+        // Find and remove item from an array
+        var i = spawnConfigs.indexOf(spawnConf);
+        if(i != -1) {
+            spawnConfigs.splice(i, 1);
+        }
+    });
+
+    // create a new process config entry and spawn it
+    app.post('/conf/spawn/create', function(req, res) {
+    });
+
+    // update the entire proxy [{}] config
+    app.post('/conf/proxy/update', function() {});
 
     /* Route to write out files to the FS.
      * Relies on forever-monitor to be watching and restart the app. */
